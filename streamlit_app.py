889648aa26188a8a5be84e9981830a67f99d6c63
Diego Carvalho <d.carvalho@ieee.org>
2022-03-24 11:58:00 -0300

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,65 @@
+""" streamlit_app.py fast geotag map app (c) 2022 Diego Carvalho
+MIT License
+
+Copyright (c) 2022 Diego Carvalho
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import streamlit as st
+import folium
+import pandas as pd
+import numpy as np
+from io import StringIO
+
+# COPYRIGHT SECTION
+__author__ = "Diego Carvalho"
+__copyright__ = "Copyright 2022, The MOBLAB Collaboration (CEFET/RJ)"
+__credits__ = ["Diego Carvalho"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Diego Carvalho"
+__email__ = "d.carvalho@ieee.org"
+__status__ = "Research"
+
+
+colnames = ["lat", "lon"]
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    # To read file as bytes:
+    data_load_state = st.text("Loading data...")
+    bytes_data = uploaded_file.getvalue()
+    # st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    # st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    colnames = ["lat", "lon"]
+    df = pd.read_csv(uploaded_file, names=colnames, header=None)
+    data_load_state.text("Loading data...done!")
+    st.subheader("Fast geotag map")
+    st.map(df, zoom=10)
